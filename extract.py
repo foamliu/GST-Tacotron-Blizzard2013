2a35c32ccf7009c2ef62cac2cd986ce5106cb0f0
Yang Liu <liuyang12@focusmedia.cn>
2019-09-20 11:31:46 +0800

    update

---
+++ extract.py
@@ -0,0 +1,12 @@
+import tarfile
+
+
+def extract(filename):
+    print('Extracting {}...'.format(filename))
+    tar = tarfile.open(filename, 'r')
+    tar.extractall('data')
+    tar.close()
+
+
+if __name__ == "__main__":
+    extract('data/Lessac_Blizzard2013_CatherineByers_train.tar.bz2')
